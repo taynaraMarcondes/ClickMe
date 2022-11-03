d260ed02ca8e34e37336510da5b82084d8370d33
taynaraMarcondes <taynaramarcondes@yahoo.com.br>
2022-11-03 14:32:11 -0300

    remove logs

--- Test/ClickMe.js
+++ Test/ClickMe.js
@@ -8,13 +8,9 @@ function buttonClicked(buttonName) {
   const header = document.getElementById("heading");
   button = buttonName ? "You just clicked '" + buttonName + "'" : '';
 
-  console.log("button: ", button)
-
   if (button != '') {
-    console.log("aquiu")
     header.innerHTML = button
   } else {
-    console.log("aqui")
     header.innerHTML = ""
 
   }
